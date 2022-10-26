9de4551ebb0db42d3764413bd984a37b38fb22ff
Aneta Rolnik <aneta.r@poczta.onet.eu>
2022-10-26 19:33:20 +0200

    Improve hero section

--- components/home-page/hero/Hero.tsx
+++ components/home-page/hero/Hero.tsx
@@ -1,21 +1,24 @@
 import Image from "next/image";
 
+import Container from "../../layout/container/Container";
 import scss from "./Hero.module.scss";
 
 const Hero = (): JSX.Element => {
   return (
     <section className={scss.hero}>
-      <Image
-        src="/images/myAvatar.png"
-        alt="My Avatar"
-        title="My Avatar"
-        width={250}
-        height={250}
-        className={scss.avatar}
-        priority
-      />
-      <div className={scss.title}>Hi, I'm Aneta</div>
-      <p className={scss.text}>This is not a real blog, but it could be.</p>
+      <Container>
+        <Image
+          src="/images/myAvatar.png"
+          alt="avatar"
+          title="Avatar"
+          width={200}
+          height={200}
+          className={scss.avatar}
+          priority
+        />
+        <div className={scss.title}>Hi, I'm Aneta</div>
+        <p className={scss.text}>This is not a real blog, but it could be.</p>
+      </Container>
     </section>
   );
 };
