afe44761e1a48024ae4450c7dea6c0d0869bbe58
Aneta Rolnik <aneta.r@poczta.onet.eu>
2022-10-26 17:46:29 +0200

    The Home Page

--- components/home-page/hero/Hero.tsx
+++ components/home-page/hero/Hero.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+
+import scss from "./Hero.module.scss";
+
 const Hero = (): JSX.Element => {
-  return <h1>Hero</h1>;
+  return (
+    <section className={scss.hero}>
+      <Image
+        src="/images/myAvatar.png"
+        alt="My Avatar"
+        title="My Avatar"
+        width={250}
+        height={250}
+        className={scss.avatar}
+        priority
+      />
+      <div className={scss.title}>Hi, I'm Aneta</div>
+      <p className={scss.text}>This is not a real blog, but it could be.</p>
+    </section>
+  );
 };
 
 export default Hero;
