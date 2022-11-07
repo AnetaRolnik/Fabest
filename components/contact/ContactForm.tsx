a48c1041829326196fa30df1f8829b12672c3166
Aneta Rolnik <aneta.r@poczta.onet.eu>
2022-11-07 19:24:26 +0100

    Send data from contact form

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -1,22 +1,64 @@
+import { use, useState } from "react";
+
 import Container from "../layout/container/Container";
 import scss from "./Contact.module.scss";
 
 const ContactForm = (): JSX.Element => {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [eneteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+
+  const sendMessageHandler = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    const body = {
+      email: enteredEmail,
+      name: eneteredName,
+      message: enteredMessage,
+    };
+
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" },
+    });
+  };
+
   return (
     <Container>
       <h1>Contact me!</h1>
-      <form>
+      <form onSubmit={sendMessageHandler}>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" required />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            required
+            value={enteredEmail}
+            onChange={(event) => setEnteredEmail(event.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={eneteredName}
+            onChange={(event) => setEnteredName(event.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" rows={5} required />
+          <textarea
+            name="message"
+            id="message"
+            rows={5}
+            required
+            value={enteredMessage}
+            onChange={(event) => setEnteredMessage(event.target.value)}
+          />
         </div>
         <button>Send Message</button>
       </form>
