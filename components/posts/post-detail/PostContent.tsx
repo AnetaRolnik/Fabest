ec681a2ce77194fe6cc1ad5eaca5a1f98563fd64
Aneta Rolnik <aneta.r@poczta.onet.eu>
2022-11-07 18:27:33 +0100

    Add single post styles and more posts

--- components/posts/post-detail/PostContent.tsx
+++ components/posts/post-detail/PostContent.tsx
@@ -2,6 +2,7 @@ import ReactMarkdown from "react-markdown";
 import Image from "next/image";
 
 import Container from "../../layout/container/Container";
+import scss from "./PostContent.module.scss";
 
 type Props = {
   content: string;
@@ -12,19 +13,31 @@ const PostContent = (props: Props): JSX.Element => {
   const { content, slug } = props;
 
   const customComponent = {
-    img: (image: { src?: string; alt?: string; title?: string }) => (
-      <Image
-        src={`/images/posts/${slug}/${image.src}`}
-        alt={image.alt ? image.alt : slug}
-        title={image.title ? image.alt : slug}
-        width={600}
-        height={300}
-      />
-    ),
+    p(paragraph: any) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={scss.imgWrapper}>
+            <Image
+              src={`/images/posts/${slug}/${image.properties.src}`}
+              alt={image.alt}
+              title={image.title}
+              width={600}
+              height={400}
+              className={scss.img}
+            />
+          </div>
+        );
+      }
+      return <p className={scss.paragraph}>{paragraph.children}</p>;
+    },
   };
 
   return (
-    <Container>
+    <Container className={scss.content}>
       <ReactMarkdown components={customComponent}>{content}</ReactMarkdown>
     </Container>
   );
